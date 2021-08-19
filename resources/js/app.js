1d0c44874616c0229109dfb955afd58dac3c3b7c
Omar20Ashraf <omaarashraf2020@gmail.com>
2021-08-19 15:17:06 +0200

    install dependance

---
+++ resources/js/app.js
@@ -0,0 +1,18 @@
+require('./bootstrap');
+
+import Vue from 'vue'
+
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+window.Vue = Vue
+
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+
+const app = new Vue({
+    el: '#app',
+});
+
